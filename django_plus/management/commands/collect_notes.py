8171ee1bc0d9297b90e0393384d635cbcecd83fd
Zadigo <pendenquejohn@gmail.com>
2026-05-28 00:09:57 +0200

    Implement collect notes

---
+++ django_plus/management/commands/collect_notes.py
@@ -0,0 +1,63 @@
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django_plus.management.utils import signalcommand
+import pathlib
+from typing import Iterator
+import re
+
+
+START_REGEX = re.compile(
+    r"\{?#[\s]*?(TODO|FIXME|BUG|HACK|WARNING|NOTE|XXX)[\s:]?(.+)"
+)
+
+END_REGEX = re.compile(
+    r"(.*)#\}(.*)"
+)
+
+
+class Command(BaseCommand):
+    help = 'Show all annotations like TODO, FIXME, BUG, HACK, WARNING, NOTE etc. in your py and HTML files.'
+    label = 'annotation tag (TODO, FIXME, BUG, HACK, WARNING, NOTE...)'
+
+    @signalcommand
+    def handle(self, *args, **options):
+        apps: list[str] = []
+        for app in getattr(settings, 'INSTALLED_APPS', []):
+            if app.startswith('django.contrib'):
+                continue
+            apps.append(app)
+
+        # template_dirs = getattr(settings, 'TEMPLATES', [])[0].get('DIRS', [])
+        base_dir: pathlib.Path = getattr(settings, 'BASE_DIR', None)
+
+        for app in apps:
+            fullpath = base_dir.joinpath(app)
+            lines = self._iterate_files(fullpath.rglob('*.py'))
+            if lines:
+                for line in lines:
+                    self.stdout.write(
+                        self.style.SUCCESS(f"   + ") + line
+                    )
+
+    def _iterate_files(self, files: Iterator[pathlib.Path]):
+        lines: list[str] = []
+
+        _files = list(files)
+        if len(_files) > 0:
+            self.stdout.write(f'Collecting notes from {len(_files)} files...')
+        
+        for file in _files:
+            with file.open() as f:
+                linenumber = 0
+                for line in f.readlines():
+                    linenumber += 1
+                    if START_REGEX.search(line):
+                        tag, message = START_REGEX.findall(line)[0]
+
+                        text = ''
+                        if END_REGEX.search(message.strip()):
+                            text = END_REGEX.findall(text.strip())[0][0]
+                        lines.append(
+                            f'{file}:{linenumber} {tag} {text.strip()}')
+        return lines
---
+++ tests/management/commands/test_collect_notes.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, override_settings
+from django.core.management import call_command
+from io import StringIO
+import pathlib
+
+
+TEST_DIR = pathlib.Path(__file__).parent.parent.parent.parent.absolute()
+
+
+@override_settings(BASE_DIR=TEST_DIR)
+class TestCollectNotesCommand(TestCase):
+    def test_collect_notes_command(self):
+        with self.settings(INSTALLED_APPS=['django_plus']):
+            out = StringIO()
+            call_command('collect_notes', stdout=out)
+            output = out.getvalue()
+            self.assertIn('Collecting notes from', output)
