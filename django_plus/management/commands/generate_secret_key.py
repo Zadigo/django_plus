277efd0ff1ac1d070a2df30c8d39982166792984
Zadigo <pendenquejohn@gmail.com>
2026-05-27 17:58:13 +0200

    Add generate secret key

---
+++ django_plus/management/commands/generate_secret_key.py
@@ -0,0 +1,16 @@
+from typing import Sequence
+
+from django.core.management.base import BaseCommand
+from django.core.management.utils import get_random_secret_key
+
+from django_plus.management.utils import signalcommand
+
+
+class Command(BaseCommand):
+    help = "Generates a new SECRET_KEY that can be used in a project settings file."
+
+    requires_system_checks: Sequence[str] = []
+
+    @signalcommand
+    def handle(self, *args, **options):
+        return f'{self.style.SUCCESS("Generated SECRET_KEY:")} {get_random_secret_key()}'
