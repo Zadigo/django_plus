ebd09149a17cbf3ed6c9eb5290078c43dbd7ce99
Zadigo <pendenquejohn@gmail.com>
2026-07-31 00:06:48 +0200

    Update collect notes tests

--- tests/management/commands/test_collect_notes.py
+++ tests/management/commands/test_collect_notes.py
@@ -2,16 +2,17 @@ import pathlib
 from io import StringIO
 
 from django.core.management import call_command
-from django.test import TestCase, override_settings
+from django.test import TestCase
 
 TEST_DIR = pathlib.Path(__file__).parent.parent.parent.parent.absolute()
 
 
-@override_settings(BASE_DIR=TEST_DIR)
+# @override_settings(BASE_DIR=TEST_DIR)
 class TestCollectNotesCommand(TestCase):
     def test_collect_notes_command(self):
-        with self.settings(INSTALLED_APPS=['django_plus']):
+        with self.settings(BASE_DIR=TEST_DIR, INSTALLED_APPS=['django_plus', 'tests.testapp']):
             out = StringIO()
             call_command('collect_notes', stdout=out)
+
             output = out.getvalue()
             self.assertIn('Collecting notes from', output)
--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -1,6 +1,7 @@
 from django_plus.db.models import TimeStampModel
 
 
+# TODO: A note for collect_notes command test
 class TimestampedTestModel(TimeStampModel):
     class Meta:
-        app_label = 'django_plus'
+        app_label = 'tests.testapp'
